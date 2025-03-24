ce477019c9295c0e08ed5332de779ea6eb358491
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2025-03-24 11:17:19 +0100

    Update rysnc-file-sync.sh

--- rysnc-file-sync.sh
+++ rysnc-file-sync.sh
@@ -13,7 +13,7 @@ DEST_PORT="22"
 DEST_PASSWORD=""
 DEST_PATH="/home/$DEST_USER/"
 
-LOCAL_TEMP_DIR="/tmp/rsync_temp"
+LOCAL_TEMP_DIR="/home/thegrtnx/Documents/"
 LOG_FILE="rsync_transfer.log"
 MAX_RETRIES=5
 
@@ -21,19 +21,32 @@ MAX_RETRIES=5
 EXCLUDE=(
     "--exclude=*.sql"
     "--exclude=wp-config.php"
-    "--exclude=.env"
     "--exclude=cpanel"
     "--exclude=mail"
     "--exclude=logs"
     "--exclude=tmp"
     "--exclude=.*"
     "--include=.htaccess"
+    "--include=.env"
     "--exclude=backup_*"
     "--exclude=*.bak"
     "--exclude=*.tar"
     "--exclude=*.tar.gz"
     "--exclude=*.zip"
     "--exclude=*.gz"
+     "--exclude=*/node_modules/"  # Exclude any node_modules folder in any directory
+    "--exclude=*/images/"  # Exclude any images folder in any directory
+    "--exclude=*/id_images/"  # Exclude any id_images folder in any directory
+    "--exclude=*/upload/"  # Exclude any upload folder in any directory
+    "--exclude=*/uploads/"  # Exclude any uploads folder in any directory
+    "--exclude=*/activation_images/"  # Exclude any activation_images folder in any directory
+    "--exclude=*/images/faces/"  # Exclude any images/faces folder in any directory
+    "--exclude=*/images/idcard/" # Exclude any images/idcard folder in any directory
+    "--exclude=*.mp4"         # Exclude mp4 files
+    "--exclude=*.pdf"         # Exclude pdf files
+    "--exclude=*.pptx"        # Exclude pptx files
+    "--exclude=*.exe"         # Exclude exe files
+    "--exclude=*.dmg"         # Exclude dmg files
 )
 
 # Function to close all existing SSH tunnels
@@ -54,9 +67,10 @@ sync_files() {
     while [ $attempt -le $MAX_RETRIES ]; do
         echo "Attempt $attempt: Pulling files from source..." | tee -a "$LOG_FILE"
 
-        sshpass -p "$SRC_PASSWORD" rsync -avzP --partial --append-verify --ignore-existing \
+        sshpass -p "$SRC_PASSWORD" rsync -avzP --protocol=31 --partial --append-verify --ignore-existing \
             -e "ssh -p $SRC_PORT" \
             "${EXCLUDE[@]}" \
+            --ignore-existing \
             "$SRC_USER@$SRC_HOST:$SRC_PATH" "$LOCAL_TEMP_DIR/" \
             | tee -a "$LOG_FILE"
 
@@ -64,9 +78,10 @@ sync_files() {
             echo "✅ Files successfully pulled from source!" | tee -a "$LOG_FILE"
 
             echo "🚀 Attempt $attempt: Pushing files to destination..." | tee -a "$LOG_FILE"
-            sshpass -p "$DEST_PASSWORD" rsync -avzP --partial --append-verify --ignore-existing \
+            sshpass -p "$DEST_PASSWORD" rsync -avzP --protocol=31 --partial --append-verify --ignore-existing \
                 -e "ssh -p $DEST_PORT" \
                 "${EXCLUDE[@]}" \
+                --ignore-existing \
                 "$LOCAL_TEMP_DIR/" "$DEST_USER@$DEST_HOST:$DEST_PATH" \
                 | tee -a "$LOG_FILE"
 
